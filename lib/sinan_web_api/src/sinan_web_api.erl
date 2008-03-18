bc251010656f23a4a219d01047820810e1283029
Eric Merritt <cyberlync@gmail.com>
2008-03-17 22:03:03 -0700

    initial pass at crary integration
    This adds the initial parts of the rest api

---
+++ lib/sinan_web_api/src/sinan_web_api.erl
@@ -0,0 +1,56 @@
+%% -*- mode: Erlang; fill-column: 132; comment-column: 118; -*-
+%%%-------------------------------------------------------------------
+%%% Copyright (c) 2007 Erlware
+%%%
+%%% Permission is hereby granted, free of charge, to any
+%%% person obtaining a copy of this software and associated
+%%% documentation files (the "Software"), to deal in the
+%%% Software without restriction, including without limitation
+%%% the rights to use, copy, modify, merge, publish, distribute,
+%%% sublicense, and/or sell copies of the Software, and to permit
+%%% persons to whom the Software is furnished to do so, subject to
+%%% the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall
+%%% be included in all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+%%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+%%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+%%% NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+%%% HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+%%% WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+%%% OTHER DEALINGS IN THE SOFTWARE.
+%%%---------------------------------------------------------------------------
+%%% @author Eric Merritt <ericmerritt@erlware.org>
+%%% @copyright (C) 2008, Eric Merritt
+%%% @doc
+%%%  Provides an  interface into the sinan web api. Mostly used at this
+%%%  point for starting and stopping the system.
+%%% @end
+%%% Created : 15 Mar 2008 by Eric Merritt <ericmerritt@erlware.org>
+%%%-------------------------------------------------------------------
+-module(sinan_web_api).
+
+%% API
+-export([start/0]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+start() ->
+    sinan:start(),
+    application:start(crary),
+    application:start(sinan_web_api).
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
