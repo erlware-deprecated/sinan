d3b0701c7a8b873bb55ac35899c83a51f5d6e089
Eric Merritt <cyberlync@gmail.com>
2008-04-09 23:28:24 -0700

    converted args.py to a more functional representation
    This greatly simplifys the arg parsing routines and removes explicit but cluttering state.

--- client/libsinan/args.py
+++ client/libsinan/args.py
@@ -1,145 +1,126 @@
 import re
 from sinexceptions import SinanError
 
+VALUE_CHECKER = re.compile("\w+")
+
 class ParseError(SinanError):
     def __init__(self, value):
         self.value = value
 
 
-class ParsedArgs:
-    "A simple class designed to handle parsed args, and arg parsing"
-    KEY = 100
-    SPECIAL_VALUE = 150
-    VALUE = 200
-    START = 300
-
-    VALUE_CHECKER = re.compile("\w+")
-
-    def __init__(self, argv, default_task, special_opts = {}, default_opts = {}):
-        self.argv = argv
-        self.task = None
-        self.default_task = default_task
-        self.opt_root = default_opts
-        self.current_key = None
-        self.current_opt = self.opt_root
-        self.special_opts = special_opts
-
-    def parse(self):
-        """Parses the argv associated with this task into
-        consumable objects """
-
-        self.parse_state = ParsedArgs.START
-
-        for arg in self.argv:
-            self.parse_arg(arg)
-
-        if self.parse_state == ParsedArgs.SPECIAL_VALUE:
-            self.special_opts[self.special_key] = True
-
-    def parse_task(self, arg):
-        """ Just set the task if it doesn't already exist.
-        Otherwise error out"""
-        if self.task == None:
-            self.task = arg
-        else:
-            raise ParseError("Only expected one task arg, got " + self.task +
-                             " and " + arg)
-
-    def parse_key_part(self, start, arg):
-        pos = arg.find(":", start)
-
-        if pos == -1:
-            self.current_key = arg[start:]
-            self.parse_state = ParsedArgs.VALUE
-        else:
-            key = arg[start:pos]
-            Obj = {}
-            try:
-                Obj = self.current_opt[key]
-            except KeyError:
-                self.current_opt[key] = Obj
-
-            self.current_opt = Obj
-            self.parse_key_part(pos + 1, arg)
+def parse_task(argv, arg, args, index):
+    """ Just set the task if it doesn't already exist.
+    Otherwise error out"""
+    try:
+        args['task']
+        raise ParseError("Only expected one task arg, got " + self.task +
+                         " and " + arg)
+    except KeyError:
+        args['task'] = arg
+        parse_possible_key(argv, args, index + 1)
 
 
-
-    def parse_key(self, arg):
-        self.parse_key_part(0, arg)
-        self.parse_state = ParsedArgs.VALUE
-
-
-    def parse_special_key(self, arg):
-        """ Parse special local keys, these are keys
-        for the client not the server """
-        self.special_key = arg
-        self.parse_state = ParsedArgs.SPECIAL_VALUE
-
-    def parse_possible_key(self, arg):
-        if arg.startswith("--"):
-            self.parse_key(arg[2:])
-        elif arg.startswith("-"):
-            self.parse_key(arg[1:])
-        elif arg.startswith("+"):
-            self.parse_special_key(arg[1:])
-        else:
-            self.parse_task(arg)
-
-    def parse_special_value(self, arg):
-        if arg.startswith("+") or arg.startswith("-"):
-            self.special_opts[self.special_key] = True
-            self.special_key = None
-            self.parse_state = ParsedArgs.KEY
-            self.parse_possible_key(arg)
-        else:
-            self.parse_state = ParsedArgs.KEY
-            self.special_opts[self.special_key] = arg
-
-    def parse_value(self, arg):
-        """Parse the value. Really just make sure it doesn't look
-        like a key. if it does error out"""
-        if arg.startswith("-"):
-            raise ParseError("Expecting value, got arg")
-        if self.current_key == None:
-            raise ParseError("No key specified")
-
-        if ParsedArgs.VALUE_CHECKER.match(arg):
-            self.current_opt[self.current_key] = arg
-        else:
-            self.current_opt[self.current_key] = simplejson.load(arg)
-
-        self.current_opt = self.opt_root
-        self.parse_state = ParsedArgs.KEY
+def parse_special_key(argv, arg, args, index):
+    """ Parse special local keys, these are keys
+    for the client not the server """
+    parse_special_value(argv, arg, args, index + 1)
 
 
-    def parse_arg(self, arg):
-        """ This is a simple parse state dispatcher,
-        all the actual work is done in the various parse methods """
+def parse_special_value(argv, key, args, index):
+    if (len(argv) <= index or
+        argv[index].startswith("+") or
+        argv[index].startswith("-")):
+        args['special_opts'][key] = True
+        parse_possible_key(argv, args, index)
+    else:
+        arg = argv[index]
+        args['special_opts'][key] = arg
+        parse_possible_key(argv, args, index + 1)
 
-        {ParsedArgs.KEY: self.parse_possible_key,
-         ParsedArgs.SPECIAL_VALUE: self.parse_special_value,
-         ParsedArgs.VALUE: self.parse_value,
-         ParsedArgs.START: self.parse_possible_key} [self.parse_state](arg)
+
+def parse_key(argv, arg, start, current_opt, args, index):
+    pos = arg.find(":", start)
+
+    if pos == -1:
+        parse_value(argv, arg[start:], current_opt, args, index + 1)
+    else:
+        key = arg[start:pos]
+        obj = {}
+        try:
+            obj = current_opt[key]
+        except KeyError:
+            current_opt[key] = obj
+
+        parse_key(argv, arg, pos + 1, obj, args, index)
+
+
+def parse_value(argv, key, opt, args, index):
+    """Parse the value. Really just make sure it doesn't look
+    like a key. if it does error out"""
+    if len(argv) <= index:
+        raise ParseError("Expected a value for " + key)
+
+    arg = argv[index]
+
+    if arg.startswith("-"):
+        raise ParseError("Expecting value, got arg")
+
+    if VALUE_CHECKER.match(arg):
+        opt[key] = arg
+    else:
+        opt[key] = simplejson.load(arg)
+
+    parse_possible_key(argv, args, index + 1)
+
+
+def parse_possible_key(argv, args, index):
+    if len(argv) <= index:
+        return args
+
+    arg = argv[index]
+    if arg.startswith("--"):
+        parse_key(argv, arg[2:], 0, args['opts'], args, index)
+    elif arg.startswith("-"):
+        parse_key(argv, arg[1:], 0, args['opts'], index)
+    elif arg.startswith("+"):
+        parse_special_key(argv, arg[1:], args, index)
+    else:
+        parse_task(argv, arg, args, index)
+
 
 
 def parse(argv, default_task, special_opts = {}, default_opts = {}):
     """ Init the parsed args and return an arg object representing them
 
-    >>> parse(['--zu:za:zee', 'ahah'], 'ha').opt_root
+    >>> parse(['--zu:za:zee', 'ahah'], 'ha')['opts']
     {'zu': {'za': {'zee': 'ahah'}}}
 
-    >>> parse(['--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah'], 'ha').opt_root
+    >>> parse(['--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah'], 'ha')['opts']
     {'zu': {'za': {'zook': 'muhahah', 'zee': 'ahah'}}}
 
-    >>> parse(['--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah', '+port', '3322'], 'ha').special_opts
+    >>> parse(['--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah', '+port', '3322'], 'ha')['special_opts']
     {'port': '3322'}
 
-    >>> parse(['--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah', '+help', '+port', '3322'], 'ha').special_opts
+    >>> parse(['--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah', '+help', '+port', '3322'], 'ha')['special_opts']
     {'port': '3322', 'help': True}
+
+    >>> parse(['hobo', '--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah', '+help', '+port', '3322'], 'ha')['task']
+    'hobo'
+
+    >>> parse(['--zu:za:zee', 'ahah', '--zu:za:zook', 'muhahah', '+help', '+port', '3322'], 'ha')['task']
+    'ha'
     """
-    parser = ParsedArgs(argv, default_task, special_opts, default_opts)
-    parser.parse()
-    return parser
+    args = {'special_opts': special_opts,
+            'opts': default_opts,
+            'default_task': default_task}
+    parse_possible_key(argv, args, 0)
+
+    try:
+        args['task']
+    except KeyError:
+        args['task'] = default_task
+
+    return args
 
 
 
