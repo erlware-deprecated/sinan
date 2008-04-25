1a4182e896e6f88bbe9c0aa7593c00f8b12ec95a
Eric Merritt <cyberlync@gmail.com>
2008-04-25 00:02:08 -0700

    plugin support for task handlers, complete gen task client
    This provides complete plugin support for the sinan client system. It also adds a complete gen task client as the first plugin.

--- client/libsinan/gen_handler.py
+++ client/libsinan/gen_handler.py
@@ -1,8 +1,93 @@
+import libsinan.handler
+import re
 
-class GenHandler:
+class GenHandler(libsinan.handler.Handler):
+    DEFAULT_REPO = "http://repo.erlware.org/writable"
 
-    def __init__():
+    def handles(self, task):
+        return task == "gen"
+
+    def valid_repo(self, repo):
+        return not repo.strip() == ""
+
+    def get_repositories(self):
+        print "Please specify the locations of the repositories. "
+        value = self.ask_user('repository', self.DEFAULT_REPO)
+        values = [value]
+
+        more = self.ask_user('would you like to enter another y/n', 'n').upper()
+        if more == 'Y' or more == 'YES':
+            while 1:
+                value = self.ask_user('repository')
+                values.append(value)
+                more = self.ask_user('would you like to enter another y/n',
+                                     'n').upper()
+                if more == 'N' or more == 'NO':
+                    break
+        return values
 
 
-add_task_handler(GenHandler)
+    def gather_user_info(self):
+        print "Please specify your name"
+        name = self.ask_user("your name")
+        print "Please specify your email address"
+        address = self.ask_user('your email')
+        print "Please specify the copyright holder"
+        copyholder = self.ask_user('copyright holder', name)
 
+        if not copyholder:
+            copyholder = name
+
+        return {"username" : name,
+                "email_address" : address,
+                "copyright_holder" : copyholder}
+
+
+    def get_application_names(self):
+        print ("Please specify the names of the OTP apps" +
+        " that belong to this project. ")
+        value = self.ask_user('app')
+        values = [value]
+
+        more = self.ask_user('would you like to enter another y/n', 'n').upper()
+        if more == 'Y' or more == 'YES':
+            while 1:
+                value = self.ask_user('app')
+                values.append(value)
+                more = self.ask_user('would you like to enter another y/n',
+                                     'n').upper()
+                if more == 'N' or more == 'NO':
+                    break
+        return values
+
+
+
+    def get_new_project_info(self):
+        print "Please specify name of your project"
+        name = self.ask_user('project name')
+        print "Please specify the version of your project \n"
+        version = self.ask_user('project version')
+
+        return {"project_version" : version,
+                "project_name" : name}
+
+
+    def handle(self, largs):
+        """ Query the user for information about the user and
+        the project he wishes to create before passing that
+        information to the user """
+        largs['opts']['tasks'] = {"gen" :
+                                  {"user_info" : self.gather_user_info(),
+                                   "repositories" : self.get_repositories(),
+                                   "project_info" :
+                                   self.get_new_project_info(),
+                                   "apps" : self.get_application_names()}}
+
+
+
+        self.do_request(largs)
+
+
+
+
+libsinan.add_task_handler(GenHandler())
---
+++ client/libsinan/handler.py
@@ -0,0 +1,63 @@
+import libsinan.output
+import re
+import os
+import simplejson
+import urllib2
+
+class Handler:
+    DEFAULT_VALIDATOR = re.compile('\w+')
+
+
+    def ask_user(self, prompt, default = None, regexp = DEFAULT_VALIDATOR):
+        if default:
+            prompt += ' [default ' + default + ']'
+
+        prompt = prompt + "> "
+        value = raw_input(prompt)
+
+        if not value and default:
+            return default
+
+        while not regexp.match(value):
+            print value + " wasn't valid!"
+            value = raw_input(prompt)
+        return value
+
+    def add_start_dir(self, config):
+        """ Add the argument required by many or most of
+        the tasks in the system. """
+        try:
+            config['opts']['build']['start_dir'] = os.getcwd()
+        except KeyError:
+            config['opts']['build'] = {'start_dir' : os.getcwd()}
+        return config
+
+    def handles(self):
+        return true
+
+
+    def jsonify_opts(self, largs):
+        """ The opts are already in config layout. All we need to
+        do is jsonify them """
+        try:
+            return simplejson.dumps(largs['opts'])
+        except KeyError:
+            return None
+
+    def do_request(self, largs, handle = libsinan.output.handle):
+        """ Actually make the task request to the server """
+        config = self.jsonify_opts(self.add_start_dir(largs))
+        print config
+        task = largs['task']
+        url = largs['special_opts']['url']
+        if url[-1:] == '/':
+            url += 'do_task/' + task
+        else:
+            url += '/do_task/' + task
+
+        conn = urllib2.urlopen(url, config)
+
+        handle(task, conn)
+
+    def handle(self, largs):
+        self.do_request(largs)
