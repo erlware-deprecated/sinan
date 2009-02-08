522fd099521ff2d06c4b80538601802183fd56d6
Eric Merritt <cyberlync@gmail.com>
2009-02-08 11:10:40 -0600

    Client side support of versioning

---
+++ client/libsinan/version_check_handler.py
@@ -0,0 +1,41 @@
+import libsinan
+
+from libsinan import handler, output, jsax
+
+
+class VersionCheckTaskHandler(output.SimpleTaskHandler):
+
+    def object_end(self):
+        """ We only get one object per right now so
+        lets print it out when we get it """
+
+        if self.task == "version":
+            if self.event_type == 'info':
+                self.version = self.desc
+            return True
+        else:
+            return output.SimpleTaskHandler.object_end(self)
+
+
+class VersionCheckHandler(handler.Handler):
+
+    def handles(self, task):
+        return task == "version"
+
+    def handle(self, largs):
+        self.do_request(largs, handle)
+
+version = None
+
+def handle(task, conn):
+    global version
+    if conn.status == 200:
+        try:
+            task_handler = VersionCheckTaskHandler()
+            jsax.parse(conn, task_handler)
+            version = task_handler.version
+            return 0
+        except ValueError, msg:
+            print "Got an error back from sinan. Check the logs at ~/.sinan/logs/kernel.log"
+    else: 
+        return 1
