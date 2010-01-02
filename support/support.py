9d4750ef08c2d65a2c03e1b032e577ed542576e2
Eric Merritt <cyberlync@gmail.com>
2010-01-02 15:07:11 -0600

    version bump to 0.16.2.1

--- support/support.py
+++ support/support.py
@@ -21,7 +21,7 @@ ERLWARE_PATH = "/usr/local/erlware"
 ERLC = "erlc +debug_info "
 
 LOCAL_APPS = [("etask", "0.6.2"),
-              ("sinan", "0.16.2.0"),
+              ("sinan", "0.16.2.1"),
               ("sinan_web_api", "0.1.0.6")]
 
 ERLWARE_APPS = [
