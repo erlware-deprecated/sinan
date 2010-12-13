63d3bda160a512a2bafb618b2466428d9d17581c
Eric Merritt <ericbmerritt@gmail.com>
2010-12-13 10:33:37 -0500

    refactor sin_discover
    Refactor sin_discover, add tests add specs, refactor documentation
    
    Signed-off-by: Jordan Wilberding <jwilberding@gmail.com>

--- src/sin_discover.erl
+++ src/sin_discover.erl
@@ -31,7 +31,10 @@
 %%%--------------------------------------------------------------------------
 -module(sin_discover).
 
+-include_lib("eunit/include/eunit.hrl").
 -include("internal.hrl").
+
+-compile(export_all).
 
 %% API
 -export([discover/2]).
@@ -40,48 +43,26 @@
 %% API
 %%====================================================================
 
-%%--------------------------------------------------------------------
 %% @doc
 %%  Run the discover task.
-%% @spec discover(ProjectDir, Config) -> ok
 %% @end
-%%--------------------------------------------------------------------
+-spec discover(ProjectDir::string(), Config::sin_build_config:build_config()) ->
+    UpdatedConfig::sin_build_config:config().
 discover(ProjectDir, Config) ->
     {ok, BuildDir} = dict:find("build_dir", Config),
     AppDirs = look_for_app_dirs(Config, BuildDir, ProjectDir),
-    NewConfig = build_app_info(Config, AppDirs, []),
-    get_repo_location(NewConfig).
-
+    build_app_info(Config, AppDirs, []).
 
 %%====================================================================
 %%% Internal functions
 %%====================================================================
-%%-------------------------------------------------------------------
-%% @doc
-%%   Find the location of the repository.
-%% @spec (Config) -> ok
-%% @end
-%% @private
-%%-------------------------------------------------------------------
-get_repo_location(Config) ->
-    Var = os:getenv("SINAN_REPO"),
-    get_repo_location(Config, Var).
 
-get_repo_location(Config, false) ->
-    Repo = filename:join([os:getenv("HOME"), ".sinan", "repo"]),
-    filelib:ensure_dir(filename:join([Repo, "tmp"])),
-    dict:store("project.repository", Repo, Config);
-get_repo_location(Config, Loc) ->
-    filelib:ensure_dir(filename:join([Loc, "tmp"])),
-    dict:store("project.repository", Loc, Config).
-
-%%-------------------------------------------------------------------
 %% @doc
 %%   Given a list of app dirs retrieves the application names.
 %%
-%% @spec (Config::dict(), List::list(), Acc::list()) -> AppNames
 %% @end
-%%-------------------------------------------------------------------
+-spec build_app_info(Config::sin_build_config:build_config(), List::list(), Acc::list()) ->
+    Config::sin_build_config:build_config().
 build_app_info(Config, [H|T], Acc) ->
     AppName = filename:basename(H),
     AppFile = filename:join([H, "ebin", string:concat(AppName, ".app")]),
@@ -105,15 +86,12 @@ build_app_info(Config, [H|T], Acc) ->
 build_app_info(Config, [], Acc) ->
     dict:store("project.applist", Acc, Config).
 
-%%--------------------------------------------------------------------
 %% @doc
 %%  Convert the details list to something that fits into the config
 %%  nicely.
-%%
-%% @spec (BaseKey::string(), List::list(), Config::dict()) -> NewConfig
 %% @end
-%% @private
-%%--------------------------------------------------------------------
+-spec process_details(BaseKey::string(), List::list(), Config::dict()) ->
+    NewConfig::sin_build_config:build_config().
 process_details(BaseName, [{Key, Value} | T], Config) when is_atom(Key) ->
     process_details(BaseName, T, dict:store(BaseName ++ atom_to_list(Key),
                                             Value, Config));
@@ -122,17 +100,14 @@ process_details(BaseName, [{Key, Value} | T], Config) when is_list(Key)->
 process_details(_, [], Config) ->
     Config.
 
-%%-------------------------------------------------------------------
 %% @doc
 %%  Roles through subdirectories of the build directory looking
 %%  for directories that have a src and an ebin subdir. When it
 %%  finds one it stops recursing and adds the directory to the list
 %%  to return.
-%%
-%% @spec (Config, BuildDir, ProjectDir) -> AppDirs
 %% @end
-%% @private
-%%-------------------------------------------------------------------
+-spec look_for_app_dirs(Config::sin_build_config:build_config(),
+			BuildDir::string(), ProjectDir::string()) -> AppDirs::[string()].
 look_for_app_dirs(Config, BuildDir, ProjectDir) ->
     Ignorables = [BuildDir] ++ case dict:find("ignore_dirs", Config) of
 				   {ok, Value} -> Value;
@@ -159,14 +134,13 @@ look_for_app_dirs(Config, BuildDir, Parent, Sub, Ignorables, Acc) ->
     end.
 
 
-%%--------------------------------------------------------------------
 %% @doc
 %%  Process the app dir to see if it is an application directory.
 %%
-%% @spec (Config, BuildDir, Parent, Sub, Ignorables, Acc) -> ListOfDirs
 %% @end
-%% @private
-%%--------------------------------------------------------------------
+-spec process_app_dir(Config::sin_build_config:build_config(),
+		      BuildDir::string(), Parent::string(), Sub::string(),
+		      Ignorables::[string()], Acc::list()) -> ListOfDirs::[string()].
 process_app_dir(Config, BuildDir, Parent, Sub, Ignorables, Acc) ->
     Pwd = filename:join([Parent, Sub]),
     case filelib:is_dir(Pwd) of
@@ -194,15 +168,12 @@ process_app_dir(Config, BuildDir, Parent, Sub, Ignorables, Acc) ->
             Acc
     end.
 
-%%-------------------------------------------------------------------
 %% @doc
 %%  Given a directory checks of the name is src or ebin, compares
 %%  against its state and returns an indicator if the parent is a
 %%  app dir.
-%% @spec (File, State, Type) -> {both, Other} | State
 %% @end
-%% @private
-%%-------------------------------------------------------------------
+-spec process_dirs(File::string(), FinateState::string(), src | ebin) -> src | ebin.
 process_dirs(File, F, ebin) ->
     case {filelib:is_dir(File), F} of
         {true, "src"} ->
@@ -230,4 +201,97 @@ process_dirs(File, F, Type)  ->
 %%====================================================================
 %% tests
 %%====================================================================
+copy_from_app_test_() ->
+    {setup,
+     fun build_config/0,
+     fun(_) -> ok end,
+     fun test_project_p1/1}.
 
+
+
+test_project_p1(Config) ->
+    Results = discover(filename:join(["test_data",
+				      "sin_discover",
+				      "p1"]),
+		       Config),
+    {timeout, 2,
+     [?_assertMatch(
+	 ["p1"], sin_build_config:get_value(Results, "project.applist")),
+      ?_assertMatch(
+	 p1, sin_build_config:get_value(Results, "apps.p1.name")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1/ebin/p1.app",
+	 sin_build_config:get_value(Results, "apps.p1.dotapp")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1",
+	 sin_build_config:get_value(Results,
+				    "apps.p1.basedir"))]}.
+
+test_project_p2(Config) ->
+    Results = discover(filename:join(["test_data",
+				      "sin_discover",
+				      "p2"]),
+		       Config),
+    {timeout, 2,
+     [?_assertMatch(
+	 ["p1"], sin_build_config:get_value(Results, "project.applist")),
+      ?_assertMatch(
+	 p1, sin_build_config:get_value(Results, "apps.p1.name")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1/ebin/p1.app",
+	 sin_build_config:get_value(Results, "apps.p1.dotapp")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1",
+	 sin_build_config:get_value(Results,
+				    "apps.p1.basedir"))]}.
+
+
+test_project_p3(Config) ->
+    Results = discover(filename:join(["test_data",
+				      "sin_discover",
+				      "p2"]),
+		       Config),
+    {timeout, 2,
+     [?_assertMatch(
+	 ["p1"], sin_build_config:get_value(Results, "project.applist")),
+      ?_assertMatch(
+	 p1, sin_build_config:get_value(Results, "apps.p1.name")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1/ebin/p1.app",
+	 sin_build_config:get_value(Results, "apps.p1.dotapp")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1",
+	 sin_build_config:get_value(Results,
+				    "apps.p1.basedir"))]}.
+
+test_project_p4(Config) ->
+    Results = discover(filename:join(["test_data",
+				      "sin_discover",
+				      "p2"]),
+		       Config),
+    {timeout, 2,
+     [?_assertMatch(
+	 ["p1"], sin_build_config:get_value(Results, "project.applist")),
+      ?_assertMatch(
+	 p1, sin_build_config:get_value(Results, "apps.p1.name")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1/ebin/p1.app",
+	 sin_build_config:get_value(Results, "apps.p1.dotapp")),
+      ?_assertMatch(
+	 "test_data/sin_discover/p1",
+	 sin_build_config:get_value(Results,
+				    "apps.p1.basedir"))]}.
+
+
+
+
+build_config() ->
+    sin_build_config:store(
+      sin_build_config:store(
+	sin_build_config:store(
+	  sin_build_config:store(
+	    sin_build_config:new(),
+	    "build_dir", "_build"),
+	  "ignore_dirs", ["_", "."]),
+	"project_name", "foo"),
+      "project_version", "v1").
