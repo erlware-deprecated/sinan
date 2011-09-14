2c3fd77a1915627abf57e98a9ecd58d98292cdcb
Eric Merritt <ericbmerritt@gmail.com>
2011-09-13 19:57:33 -0500

    fix test bug in release smoketest
    Signed-off-by: Jordan Wilberding <jwilberding@gmail.com>

--- support/test/tests/release_test.py
+++ support/test/tests/release_test.py
@@ -8,7 +8,7 @@ class TestFoo(st.SmokeTest):
 
 
     def gen_release_file(self, app_desc):
-       contents =  """project : {
+        contents =  """project : {
                    name : %(project_name)s
                    vsn  : "%(project_version)s"
                },
@@ -32,62 +32,61 @@ class TestFoo(st.SmokeTest):
                       }
               }
         """ % {"project_name" : app_desc.project_name,
-                      "project_version" : app_desc.project_version}
+               "project_version" : app_desc.project_version}
 
-       with open("sinan.cfg", "w") as f:
-           f.write(contents)
+        with open("sinan.cfg", "w") as f:
+            f.write(contents)
 
 
 
     def test_foo(self):
-       app_desc = st.AppDesc(user_name = "Smoke Test User",
-                             email = "noreply@erlware.org",
-                             copyright_holder = "Smoke Test Copy, LLC.",
-                             project_name = "reltest",
-                             project_version = "0.134.0.0",
-                             app_names = ["app1", "app2", "app3", "app4"])
+        app_desc = st.AppDesc(user_name = "Smoke Test User",
+                              email = "noreply@erlware.org",
+                              copyright_holder = "Smoke Test Copy, LLC.",
+                              project_name = "reltest",
+                              project_version = "0.134.0.0",
+                              app_names = ["app1", "app2", "app3", "app4"])
 
 
-       self.do_run(app_desc)
+        self.do_run(app_desc)
+
+        currentdir = os.getcwd()
+        projdir = os.path.join(currentdir, app_desc.project_name)
+        builddir = os.path.join(projdir, "_build", "development")
+        os.chdir(projdir)
+        self.gen_release_file(app_desc)
+        for r in ["r1", "r2", "r3", "r4"]:
+            def run_dist(self, child):
+                child.expect("starting: dist")
+                child.expect(pexpect.EOF)
+
+            ((st.sinan("-r %s dist" % r)(run_dist))(self))
 
 
+        self.assert_dirs_exist(builddir,
+                               ["releases", "r1-1.0"],
+                               ["releases", "r2-2.0"],
+                               ["releases", "r3-3.0"],
+                               ["releases", "r4-1.1"])
+        self.assert_files_exist(builddir,
+                                ["tar", "r1-1.0.tar.gz"],
+                                ["tar", "r2-2.0.tar.gz"],
+                                ["tar", "r3-3.0.tar.gz"],
+                                ["tar", "r4-1.1.tar.gz"],
+                                ["releases", "r1-1.0", "r1.rel"],
+                                ["releases", "r1-1.0", "r1.boot"],
+                                ["releases", "r1-1.0", "r1.script"],
+                                ["releases", "r2-2.0", "r2.rel"],
+                                ["releases", "r2-2.0", "r2.boot"],
+                                ["releases", "r2-2.0", "r2.script"],
+                                ["releases", "r3-3.0", "r3.rel"],
+                                ["releases", "r3-3.0", "r3.boot"],
+                                ["releases", "r3-3.0", "r3.script"],
+                                ["releases", "r4-1.1", "r4.rel"],
+                                ["releases", "r4-1.1", "r4.boot"],
+                                ["releases", "r4-1.1", "r4.script"])
 
-       currentdir = os.getcwd()
-       projdir = os.path.join(currentdir, app_desc.project_name)
-       builddir = os.path.join(projdir, "_build", "development")
-       os.chdir(projdir)
-       self.gen_release_file(app_desc)
-
-       for r in ["r1", "r2", "r3", "r4"]:
-           child = st.spawn("sinan -r %s dist" % r)
-           child.expect("starting: dist")
-           child.expect(pexpect.EOF)
-
-       self.assert_dirs_exist(builddir,
-                              ["releases", "r1-1.0"],
-                              ["releases", "r2-2.0"],
-                              ["releases", "r3-3.0"],
-                              ["releases", "r4-1.1"])
-       self.assert_files_exist(builddir,
-                               ["tar", "r1-1.0.tar.gz"],
-                               ["tar", "r2-2.0.tar.gz"],
-                               ["tar", "r3-3.0.tar.gz"],
-                               ["tar", "r4-1.1.tar.gz"],
-                               ["releases", "r1-1.0", "r1.rel"],
-                               ["releases", "r1-1.0", "r1.boot"],
-                               ["releases", "r1-1.0", "r1.script"],
-                               ["releases", "r2-2.0", "r2.rel"],
-                               ["releases", "r2-2.0", "r2.boot"],
-                               ["releases", "r2-2.0", "r2.script"],
-                               ["releases", "r3-3.0", "r3.rel"],
-                               ["releases", "r3-3.0", "r3.boot"],
-                               ["releases", "r3-3.0", "r3.script"],
-                               ["releases", "r4-1.1", "r4.rel"],
-                               ["releases", "r4-1.1", "r4.boot"],
-                               ["releases", "r4-1.1", "r4.script"])
-
-
-       os.chdir(currentdir)
+        os.chdir(currentdir)
 
 if __name__ == '__main__':
     unittest.main()
