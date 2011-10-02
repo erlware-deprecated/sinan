a05b9887604f698e90aca1c957ac5f8eb8fe5ab5
Eric Merritt <ericbmerritt@gmail.com>
2011-10-02 18:55:54 -0500

    add type information to the app file
    Signed-off-by: Jordan Wilberding <jwilberding@gmail.com>

--- include/sinan.hrl
+++ include/sinan.hrl
@@ -1,13 +1,13 @@
 %% @doc Describes n application, and whether or not the application is a
 %% runtime dep or compile time dep, also wether it is a project
 %% application or a dependency
--record(app, {name,
-              vsn,
-              path,
-              type,
-              project,
-              modules,
-              sources}).
+-record(app, {name :: atom(),
+              vsn :: string(),
+              path :: string(),
+              type :: runtime | compiletime,
+              project :: boolean(),
+              modules :: sin_file_info:mod(),
+              sources :: sin_file_info:mod()}).
 
 %% @doc describes a module in the system
 %%  - name: is the name of the module
